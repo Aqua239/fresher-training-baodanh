61da9ae142701ce002961d6527b8a219cae6169f
Nguyen Gia Bao Danh <ngbdanh239@gmail.com>
2026-07-01 16:04:57 +0700

    fix: render 3D F letter and correct rotation matrices

--- web_gl/web_gl_fundamental/orthographic_3d/main.js
+++ web_gl/web_gl_fundamental/orthographic_3d/main.js
@@ -2,7 +2,7 @@
 let translation = [45, 150, 0];
 let color = [Math.random(), Math.random(), Math.random(), 1];
 var rotation = [degToRad(40), degToRad(25), degToRad(325)];
-let scale = [0.8, 0.8, 0.8];
+let scale = [1, 1, 1];
 let m4 = {
     projection: function(width, height, depth) {
         return [
@@ -40,8 +40,8 @@ let m4 = {
         let s = Math.sin(angleInRadians);
         return [
         1, 0, 0, 0,
-        0, c,-s, 0,
-        0, s, c, 0,
+        0, c, s, 0,
+        0, -s, c, 0,
         0, 0, 0, 1,
         ];
     },
@@ -69,8 +69,8 @@ let m4 = {
         let c = Math.cos(angleInRadians);
         let s = Math.sin(angleInRadians);
         return [
-        c,-s, 0, 0,
-        s, c, 0, 0,
+        c, s, 0, 0,
+        -s, c, 0, 0,
         0, 0, 1, 0,
         0, 0, 0, 1,
         ];
@@ -279,7 +279,7 @@ async function main() {
     
         let primitiveType = gl.TRIANGLES;
         offset = 0;
-        let count = 18;
+        let count = 16 * 6;
         gl.drawArrays(primitiveType, offset, count);
     }
 }
@@ -290,29 +290,133 @@ function setGeometry(gl) {
   gl.bufferData(
       gl.ARRAY_BUFFER,
       new Float32Array([
-          // left column
+          // left column front
             0,   0,  0,
            30,   0,  0,
             0, 150,  0,
             0, 150,  0,
            30,   0,  0,
            30, 150,  0,
- 
-          // top rung
+
+          // top rung front
            30,   0,  0,
           100,   0,  0,
            30,  30,  0,
            30,  30,  0,
           100,   0,  0,
           100,  30,  0,
- 
-          // middle rung
+
+          // middle rung front
            30,  60,  0,
            67,  60,  0,
            30,  90,  0,
            30,  90,  0,
            67,  60,  0,
-           67,  90,  0]),
+           67,  90,  0,
+
+          // left column back
+            0,   0,  30,
+           30,   0,  30,
+            0, 150,  30,
+            0, 150,  30,
+           30,   0,  30,
+           30, 150,  30,
+
+          // top rung back
+           30,   0,  30,
+          100,   0,  30,
+           30,  30,  30,
+           30,  30,  30,
+          100,   0,  30,
+          100,  30,  30,
+
+          // middle rung back
+           30,  60,  30,
+           67,  60,  30,
+           30,  90,  30,
+           30,  90,  30,
+           67,  60,  30,
+           67,  90,  30,
+
+          // top
+            0,   0,   0,
+          100,   0,   0,
+          100,   0,  30,
+            0,   0,   0,
+          100,   0,  30,
+            0,   0,  30,
+
+          // top rung right
+          100,   0,   0,
+          100,  30,   0,
+          100,  30,  30,
+          100,   0,   0,
+          100,  30,  30,
+          100,   0,  30,
+
+          // under top rung
+          30,   30,   0,
+          30,   30,  30,
+          100,  30,  30,
+          30,   30,   0,
+          100,  30,  30,
+          100,  30,   0,
+
+          // between top rung and middle
+          30,   30,   0,
+          30,   30,  30,
+          30,   60,  30,
+          30,   30,   0,
+          30,   60,  30,
+          30,   60,   0,
+
+          // top of middle rung
+          30,   60,   0,
+          30,   60,  30,
+          67,   60,  30,
+          30,   60,   0,
+          67,   60,  30,
+          67,   60,   0,
+
+          // right of middle rung
+          67,   60,   0,
+          67,   60,  30,
+          67,   90,  30,
+          67,   60,   0,
+          67,   90,  30,
+          67,   90,   0,
+
+          // bottom of middle rung.
+          30,   90,   0,
+          30,   90,  30,
+          67,   90,  30,
+          30,   90,   0,
+          67,   90,  30,
+          67,   90,   0,
+
+          // right of bottom
+          30,   90,   0,
+          30,   90,  30,
+          30,  150,  30,
+          30,   90,   0,
+          30,  150,  30,
+          30,  150,   0,
+
+          // bottom
+          0,   150,   0,
+          0,   150,  30,
+          30,  150,  30,
+          0,   150,   0,
+          30,  150,  30,
+          30,  150,   0,
+
+          // left side
+          0,   0,   0,
+          0,   0,  30,
+          0, 150,  30,
+          0,   0,   0,
+          0, 150,  30,
+          0, 150,   0]),
       gl.STATIC_DRAW);
 }
 
